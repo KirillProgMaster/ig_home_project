f0c6c2e03629ea755ca2e89c8b95a7d34b64d224
KirillProgMaster <kirill.zarya@inbox.ru>
2021-08-24 00:34:38 +0300

    Active Navbar

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import {NavLink} from 'react-router-dom'
 import s from './Pages.module.css'
 function Header() {
-    let[condition, setCondition] = useState<boolean>(true)
+    let[condition, setCondition] = useState<boolean>(false)
     const changeCondition = () =>{
         setCondition(!condition)
         // alert(condition)
@@ -10,10 +10,13 @@ function Header() {
     return (
         <>
             <div>React homeworks: <button onClick={changeCondition}>{condition ?<span>Hide</span> : <span>Show</span>}</button></div>
-            <div className={condition ? s.header : s.activeHeader}>
-                <div className={s.headerItem}><NavLink to={'/pre-junior'}>Pre-junior</NavLink></div>
-                <div className={s.headerItem}><NavLink to={'/junior'}>Junior</NavLink></div>
-                <div className={s.headerItem}><NavLink to={'/junior+'}>Junior+</NavLink></div>
+            <div className={s.header}>
+                <ul className={condition ? s.activeHeader : s.nonActiveHeader}>
+                    <li className={s.headerItem}><NavLink to={'/pre-junior'}>Pre-junior</NavLink></li>
+                    <li className={s.headerItem}><NavLink to={'/junior'}>Junior</NavLink></li>
+                    <li className={s.headerItem}><NavLink to={'/junior+'}>Junior+</NavLink></li>
+                </ul>
+                {/*<button onClick={changeCondition}>{condition ?<span>Hide</span> : <span>Show</span>}</button>*/}
             </div>
         </>
 
